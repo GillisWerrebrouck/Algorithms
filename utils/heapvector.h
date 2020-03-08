e529baa2328c5bdab93ec8cb186776ffc761ba38
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-03-08 22:29:01 +0100

    Add functions to extract a node from a heap

--- utils/heapvector.h
+++ utils/heapvector.h
@@ -25,9 +25,9 @@ class HeapVector : public ExtendedVector<T> {
 
         void top_down_heapify();
         void bottom_up_heapify();
-        void add(int, int);
-        void remove(int);
-        void remove_root();
+        void insert(int);
+        int extract(int);
+        int extract_root();
         void replace_root(int);
 
     private:
@@ -74,19 +74,19 @@ void HeapVector<T>::bottom_up_heapify() {
     int n = this->size();
     int parent = n/2-1;
 
-    for(int i = parent; i>=0; i--) {
+    for(int i = parent; i >= 0; i--) {
         int k = i;
         int v = (*this)[k];
         bool isHeap = false;
 
         // check if the heap condition is met and 
         // if the node with index k has a child (k is not a leaf)
-        while(!isHeap && 2*k+1<=n) {
+        while(!isHeap && 2*k+1 <= n) {
             // left child of k
             int j = 2*k+1;
 
             // if two child nodes
-            if(j<(n-1)) {
+            if(j < (n-1)) {
                 // find max child node
                 if((*this)[j] < (*this)[j+1]) j++;
             }
@@ -101,6 +101,62 @@ void HeapVector<T>::bottom_up_heapify() {
             }
         }
     }
+}
+
+// O(lg(N))
+template <class T>
+void HeapVector<T>::insert(int value) {
+    (*this).push_back(value);
+    int i = this->size()-1;
+    int parent = (i-1)/2;
+
+    // trickle-up the new value
+    while(0 <= parent && (*this)[parent] < (*this)[i]) {
+        swap((*this)[parent], (*this)[i]);
+
+        i = parent;
+        parent = (parent-1)/2;
+    }
+}
+
+// O(lg(N))
+template <class T>
+int HeapVector<T>::extract(int index) {
+    // replace the ndoe to extract with the last node
+    int extracted = (*this)[index];
+    int n = this->size();
+    (*this)[index] = (*this)[n-1];
+    (*this).pop_back();
+
+    int i = index;
+    int left_child = 2*i+1;
+    int right_child = 2*i+2;
+
+    // trickle-down the new value
+    while(left_child < n && (*this)[left_child] > (*this)[i] || 
+            right_child < n && (*this)[right_child] > (*this)[i]) {
+        if((*this)[left_child] > (*this)[right_child]) {
+            swap((*this)[left_child], (*this)[i]);
+
+            i = left_child;
+            left_child = 2*i+1;
+            right_child = 2*i+2;
+        } else {
+            swap((*this)[right_child], (*this)[i]);
+
+            i = right_child;
+            left_child = 2*i+1;
+            right_child = 2*i+2;
+        }
+    }
+
+    return extracted;
+}
+
+// O(lg(N))
+template <class T>
+int HeapVector<T>::extract_root() {
+    return extract(0);
 }
 
 #endif
